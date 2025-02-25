6dcd7694b0ddfefbf3c818156de50239588838dc
Asb-RepoByte <ahmedsbahmedsb0101@gmail.com>
2025-02-25 11:17:28 +0000

    now I'm ready to start

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Student(models.Model):
+    matricule = models.IntegerField()
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.matricule}: {self.first_name}/{self.last_name}. "
